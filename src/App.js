7ad2c671b40e599932c6a7cdfdef4d64e3720f1a
MudNam <naman.mudgal2002@gmail.com>
2025-04-03 11:26:35 +0530

    testing2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello Worlfd</h1>
+        <h1>Hellos Worlfd</h1>
         <p>My first React application</p>
       </header>
     </div>
